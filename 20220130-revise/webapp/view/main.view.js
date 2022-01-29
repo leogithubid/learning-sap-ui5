a4550c4e5434e28ab65012959d8bbfb2ee8a74e9
leo <40744320+leogithubid@users.noreply.github.com>
2022-01-30 08:18:53 +1100

    11-js-view

---
+++ 20220130-revise/webapp/view/main.view.js
@@ -0,0 +1,25 @@
+//scaffolding template
+sap.ui.jsview("home.view.main",{ //view path
+    //get controller name
+    getControllerName:function(){
+        return "home.controller.main";//return address of controller
+    },
+    //create content
+    createContent:function(oController){ //using the controller build content
+        var oInput = new sap.m.Input("idInputField",{
+            width : "500px",
+            placeholder: "Please enter your name here"
+        });
+        //oInput.placeAt("inputField");
+        var oButton = new sap.m.Button("idButton",{
+            text : "Click Me",
+            icon : "sap-icon://hello-world",
+            press : function(){
+                var name = oInput.getValue();
+                alert(" Hello " + name + ", Welcome to UI5 from JS View !")
+            }
+        });
+        //oButton.placeAt("button");
+        return [oInput,oButton];//never forget to return the view or the content of view
+    }
+    })
