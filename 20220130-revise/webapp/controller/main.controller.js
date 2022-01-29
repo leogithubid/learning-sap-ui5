4479bdd5ca107d5edc19ab896bc802796798fa63
leo <40744320+leogithubid@users.noreply.github.com>
2022-01-30 08:46:13 +1100

    13-xmlview

--- 20220130-revise/webapp/controller/main.controller.js
+++ 20220130-revise/webapp/controller/main.controller.js
@@ -7,8 +7,11 @@ sap.ui.define(
             welcomeBack : function(){
                 //how to get the name? 
                 //everything is in the core - so read from the core
-                var oCore = sap.ui.getCore();
-                var oInput = oCore.byId("idInputField").getValue();
+                //var oCore = sap.ui.getCore();
+                //var oInput = oCore.byId("idInput").getValue();
+                //after you change it to XML View, you have to read it from the view
+                console.log(sap.ui.getCore());
+                var oInput = this.getView().byId("idInput").getValue();
                 alert(` Hello ${oInput}, Welcome back to UI5 from JS View !`);
             }
         });//extend controller
